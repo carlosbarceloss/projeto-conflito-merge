803483d64c42bdb563720a61042875f85202e01a
Bruno César Angst <brunocesardev@gmail.com>
2018-10-23 01:08:10 -0300

    alteração do bruno, inclui entrada de dados

---
+++ ALGORITMO MEDIA FINAL.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+
+int presenca;
+float g1, g2, final, freq, subst;
+
+// Aqui o usuário entra com a nota da G1
+printf("Qual foi sua nota da G1? \n");
+scanf("%f", &g1);
+
+// Aqui o usuário entra com a nota da G2
+printf("Qual foi sua nota da G2? \n");
+scanf("%f", &g2);
+
+}
