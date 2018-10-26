299ab7ed1a6e0c043b38b8b11cc422461c38cda4
Carlos <carlosbarcelos@rede.ulbra.br>
2018-10-26 00:46:55 -0300

    Iteração do algoritmo, adicionando codigo para funçao de soma da nota da substituiçao

---
+++ ALGORITMO MEDIA FINAL.cpp
@@ -0,0 +1,18 @@
+if (g1>g2)
+{
+	final=(g1+(subst*2))/3;
+}
+else 
+{
+	final=(subst+(g2*2))/3;
+}
+if (final>6)
+{
+	printf("Voce foi aprovado com media %.2f apos a prova de substituiçao.",final)
+}
+else
+{
+	printf("Voce nao atingiu a media necessaria para aprovaçao. (media %f)",final)
+}
+
+
